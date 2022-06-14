c9e31b90cdac048ce23950d3d214243afae92e0b
almaariz <salsa.almaariz@gmail.com>
2022-06-14 20:58:20 +0700

    FixParallaxEffect

--- Assets/Scripts/SceneManagement/MovingBackground.cs
+++ Assets/Scripts/SceneManagement/MovingBackground.cs
@@ -4,34 +4,11 @@ using UnityEngine;
 
 public class MovingBackground : MonoBehaviour
 {
-    public float speed = 1f;
-    float i;
-    public bool title = true;
-
-    [HideInInspector]public Vector3 startPosition;
+    public Rigidbody2D rb;
+    public float initSpeed = 1f;
 
     void Start()
     {
-        startPosition = transform.position;
-    }
-
-    void FixedUpdate()
-    {
-        if(title)
-        {
-            i++;
-            float newPosition = Mathf.Repeat(Time.time * speed, i);
-            transform.position = startPosition + Vector3.right * newPosition;
-        }
-        else
-        {
-            i =0;
-            float newPosition = Mathf.Repeat(Time.time * speed, i);
-            transform.position = startPosition;
-        }
-    }
-    public void SetPlay()
-    {
-        title = false;
+        rb.AddForce(Vector3.right * initSpeed);
     }
 }
